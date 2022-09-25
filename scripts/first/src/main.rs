cf7304b7089c0c471ec8ff08f4886c6c4d00f8c4
Omar Carey <omar.carey@thoughtworks.com>
2022-09-25 00:53:27 -0500

    foo

--- scripts/first/src/main.rs
+++ scripts/first/src/main.rs
@@ -1,8 +1,7 @@
 use neural_network::neu_net::builder::builder::Builder;
 use utils::mnist::MnistData;
-use criterion::*;
 
-pub fn bench(c: &mut Criterion) {
+fn main() {
     let learning_rate = 0.9;
     let training_iterations = 1;
     let label_path = "../data/t10k-labels-idx1-ubyte.gz";
@@ -13,10 +12,6 @@ pub fn bench(c: &mut Criterion) {
     let layer_nodes = vec![784, 10];
     let mut neu_net = Builder::build(&layer_nodes);
 
-    c.bench_function("train",|b| b.iter(|| neu_net.train(&mnist_data, training_iterations, learning_rate)));
-
+    neu_net.train(&mnist_data, training_iterations, learning_rate);
 
 }
-
-criterion_group!(benches, bench);
-criterion_main!(benches);
